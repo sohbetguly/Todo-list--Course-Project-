111caf075146f8756208f231f6f3b50e0b20e41e
Sohbetguly <sobtware@gmail.com>
2023-10-26 05:05:02 -0700

    added delete button and function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import "./App.css";
-import Counter from "./Counter";
 import Footer from "./Footer";
 import List from "./List";
 import Navbar from "./Navbar";
 
 function App() {
-  const title = "This is title";
-  const parTitle = "This is paragraph title";
   return (
     <div className="App">
-      <Navbar myTitle={title} />
-      <h1>{title}</h1>
+      {/* <Navbar /> */}
       {/* <Counter /> */}
       <List />
-      <Footer parTitle={parTitle} />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/List.jsx
+++ src/List.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import { BsFillTrash2Fill } from "react-icons/bs";
 import "./List.css";
 
 const List = () => {
-  const cars = [
+  const [cars, setCars] = useState([
     "Audi",
     "BMW",
     "Mercedes",
@@ -11,14 +12,62 @@ const List = () => {
     "Honda",
     "Hudai",
     "Ford",
-  ];
+  ]);
+
+  const [value, setValue] = useState("");
+
+  const changeHandler = (event) => {
+    setValue(event.target.value);
+  };
+
+  const addCarHandler = () => {
+    if (value === "") {
+      return alert("Bosh yzdynyz");
+    }
+
+    if (!cars.includes(value)) {
+      const newCars = [value, ...cars];
+      setCars(newCars);
+      setValue("");
+    } else {
+      alert(
+        "Yazyan masynynyz ozal sanowa girizilen, bashga at yazmagynyzy hayysh edyaris"
+      );
+    }
+  };
+
+  const deleteHandler = (carName) => {
+    const deletedCarList = cars.filter((car) => car !== carName);
+    setCars(deletedCarList);
+  };
 
   return (
     <div className="list__container">
-      <h2>List of Cars</h2>
+      <div className="add-car">
+        <h2>Add a new car</h2>
+        <input
+          value={value}
+          onChange={changeHandler}
+          placeholder="car name"
+          type="text"
+        />
+        <br />
+        <button onClick={addCarHandler}>Add</button>
+      </div>
+
+      <h1>List of Cars</h1>
+
       <ul>
         {cars.map((carName) => {
-          return <li key={carName}>{carName}</li>;
+          return (
+            <li key={carName}>
+              {carName}
+              <BsFillTrash2Fill
+                onClick={() => deleteHandler(carName)}
+                className="list-icon"
+              />
+            </li>
+          );
         })}
       </ul>
     </div>
